e2212829a55a8f769c0de49de64e77ca8176f820
treefarm-mihyang <mihyang@treefarm.co.kr>
2024-11-05 17:10:43 +0900

    feat: build context

--- lib/main.dart
+++ lib/main.dart
@@ -26,17 +26,19 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
       home: Scaffold(
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text(
-                "Click here",
-                style: TextStyle(
-                  fontSize: 32,
-                ),
-              ),
+              const MyLargeTitle(),
               Text(
                 "$counter",
                 style: const TextStyle(
@@ -50,6 +52,23 @@ class _AppState extends State<App> {
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "My Large Title",
+      style: TextStyle(
+        fontSize: 32,
+        color: Theme.of(context).textTheme.titleLarge?.color,
       ),
     );
   }
