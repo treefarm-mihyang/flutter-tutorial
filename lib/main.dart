511c4273c90c09856b164c45b7088a65a4b67393
treefarm-mihyang <mihyang@treefarm.co.kr>
2024-11-07 17:24:21 +0900

    feat: flutter pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+
+const assetsImagePath = "assets/images";
+const bannerImage = "$assetsImagePath/banner.png";
 
 void main() async {
   runApp(const App());
@@ -23,10 +27,36 @@ class App extends StatelessWidget {
 class Body extends StatelessWidget {
   const Body({super.key});
 
-  onChanged() {}
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const RandomWords(),
+        Center(
+          child: Image.asset(bannerImage),
+        )
+      ],
+    );
+  }
+}
+
+class RandomWords extends StatelessWidget {
+  const RandomWords({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final wordList = generateWordPairs().take(5).toList();
+    final widgets = wordList
+        .map(
+          (word) => Text(
+            word.asUpperCase,
+            style: const TextStyle(fontSize: 32),
+          ),
+        )
+        .toList();
+
+    return Column(
+      children: widgets,
+    );
   }
 }
