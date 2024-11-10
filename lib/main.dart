aa2f63f9fdb5e02a2bc7a7ddc6cca4824e180332
treefarm-mihyang <mihyang@treefarm.co.kr>
2024-11-10 12:08:08 +0900

    chore: initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-
-import 'package:toonflix/router/router.dart';
-import 'package:toonflix/style/theme.dart';
 
 void main() async {
   runApp(const App());
@@ -13,30 +9,10 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: router,
-      theme: theme,
-    );
-  }
-}
-
-class HomeWidget extends StatelessWidget {
-  HomeWidget({
-    super.key,
-  });
-
-  final textTheme = theme.textTheme;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Router 2'),
-      ),
-      body: Center(
-        child: TextButton(
-          onPressed: () => context.pushNamed('new'),
-          child: const Text('Go to Page'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("가위 바위 보"),
         ),
       ),
     );
