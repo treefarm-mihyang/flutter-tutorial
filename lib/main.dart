f14e253ceea78ffe9d9ead60e192c6d036ff8b56
treefarm-mihyang <mihyang@treefarm.co.kr>
2024-11-10 10:45:48 +0900

    feat: flutter router

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,54 @@ void main() async {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  late int index;
+
+  @override
+  void initState() {
+    super.initState();
+    index = 0;
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Container(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Flutter Router"),
+        ),
+        body: homeBody(),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home)),
+            BottomNavigationBarItem(icon: Icon(Icons.search)),
+            BottomNavigationBarItem(icon: Icon(Icons.person)),
+          ],
+          currentIndex: index,
+          onTap: (value) => setState(() {
+            index = value;
+          }),
+        ),
+      ),
     );
+  }
+
+  Widget homeBody() {
+    switch (index) {
+      case 1:
+        return const Center(child: Icon(Icons.search));
+      case 2:
+        return const Center(child: Icon(Icons.person));
+      case 0:
+      default:
+        return const Center(child: Icon(Icons.home));
+    }
   }
 }
