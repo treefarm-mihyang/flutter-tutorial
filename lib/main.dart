b5e938358e518cbc62d230c34a047f817e679c84
treefarm-mihyang <mihyang@treefarm.co.kr>
2024-11-10 21:26:26 +0900

    chore: initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:toonflix/game/game_body.dart';
 
 void main() async {
-  runApp(const App());
+  runApp(const InstagramApp());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
+class InstagramApp extends StatelessWidget {
+  const InstagramApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("가위 바위 보"),
-        ),
-        body: const GameBody(),
-      ),
+    return const MaterialApp(
+      home: Placeholder(),
     );
   }
 }
